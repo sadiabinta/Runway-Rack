6abef4dfd4d52549a70e5eb450727554694d41ac
sadiabinta <sadiaa.kabir@gmail.com>
2023-10-27 00:43:04 -0400

    search...

--- Runway-Rack-TK/src/pages/Home/Home/Home.jsx
+++ Runway-Rack-TK/src/pages/Home/Home/Home.jsx
@@ -3,14 +3,18 @@ import DestinationBanner from "../DestinationBanner/DestinationBanner";
 import FilterDestinationCard from "../FilterDestinationCard/FilterDestinationCard";
 import Flightoption from "../FlightOption/Flightoption";
 import LatestUpdate from "../LatestUpdate/LatestUpdate";
+import Search from "../Search/Search";
+import SearchTest from "../SearchTest/SearchTest";
 
 
 
 const Home = () => {
     return (
         <div>
+
+            <SearchTest></SearchTest>
             <Banner></Banner>
-            {/* <ContactCard></ContactCard> */}
+            <Search></Search>
             <DestinationBanner></DestinationBanner>
             <FilterDestinationCard></FilterDestinationCard>
             <Flightoption></Flightoption>
